9e57609b30974415c8b7a6dad59a6b2fa0c80da6
Karan <karanrajmehta06@gmail.com>
2023-06-19 19:55:45 +0530

    Fix: nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -10,8 +10,8 @@ export default async function Navbar() {
   const session = await getServerSession(authOptions);
 
   return (
-    <div className='fixed inset-x-0 top-0 z-[10] h-fit border-b border-zinc-300 bg-zinc-100 py-2'>
-      <div className='container mx-auto flex h-full max-w-7xl items-center justify-between gap-2'>
+    <div className='fixed inset-x-0 top-0 z-[10] h-[64px] border-b border-zinc-300 bg-zinc-100/50 backdrop-blur-sm backdrop-saturate-150'>
+      <div className='container mx-auto flex h-full max-w-7xl items-center justify-between'>
         <Link href='/' className='flex items-center gap-2'>
           <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' />
           <p className='hidden text-sm font-medium text-zinc-700 md:block'>
@@ -23,7 +23,7 @@ export default async function Navbar() {
         ) : (
           <Link
             href='/sign-in'
-            className={buttonVariants({ variant: 'twitter' })}
+            className={`${buttonVariants({ variant: 'twitter' })} shrink-0`}
           >
             Sign In
           </Link>
